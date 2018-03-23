e90ca96204d5baa099d929d6c48be5ff7da03b4b
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-03-22 22:58:00 -0700

    next version

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var vampireArray = [
+	{
+		name: 'Dracula',
+		photo: "https://nerdist.com/wp-content/uploads/2017/04/bela-dracula.jpg",
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Nosferatu',
+		photo: "https://3m7ajlsrzj92lfd1hu16hu7vc-wpengine.netdna-ssl.com/wp-content/uploads/2015/11/nosferatu-660x350-1448268229.jpg",
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Selene',
+		photo: "http://cdn1-www.craveonline.com/assets/mandatory/legacy/2016/05/man_file_1067374_20_UW_Beckinsale_eyes.jpg",
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bella',
+		photo: "https://upload.wikimedia.org/wikipedia/en/7/77/Bella_Swan.jpg",
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+];
+
+module.exports = vampireArray;
